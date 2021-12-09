e172a7390b086362f2e693195a9288a8beb98c6a
Naman Sharma <officialnmn@gmail.com>
2021-12-09 10:37:42 +0530

    Your Message

--- pages/about.vue
+++ pages/about.vue
@@ -15,7 +15,7 @@
             </strong>
           </p>
           <p>
-           Hi ! I am Naman 
+            Hi
           </p>
 
           <h3>Another Fake Heading</h3>
@@ -26,24 +26,15 @@
             ratione rem repudiandae sit!
           </p>
           <quote-card />
-          <h3>Still More Fake Content</h3>
-          <p>
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Adipisci
-            eligendi impedit inventore ipsa laboriosam libero magnam modi odio,
-            sunt temporibus. Distinctio doloremque eum magni nostrum quasi
-            ratione rem repudiandae sit! Lorem ipsum dolor sit amet, consectetur
-            adipisicing elit.
-            <strong>Alias aut facere harum ipsam</strong> quibusdam quidem
-            quisquam saepe? Alias architecto blanditiis consequatur consequuntur
-            dicta ipsa iste modi provident, quod, similique veritatis.
-          </p>
+         
+          
         </div>
 
         <div class="tile is-ancestor">
           <div class="tile is-parent">
             <article class="tile is-child box">
               <p class="title">
-                Cool Stuff We Do
+                Cool Stuff We Yes we do 
               </p>
               <p class="subtitle">
                 Sooo Cool
@@ -82,17 +73,13 @@
         </div>
       </template>
 
-      <template v-slot:sidebar>
-        <h3 class="subtitle is-4">
-          Latest Posts
-        </h3>
-        <posts-grid :per-row="1" :number="2" />
-      </template>
+     
     </main-section>
   </div>
 </template>
 <script>
 import QuoteCard from '~/components/cards/QuoteCard'
+
 export default {
   head() {
     return {
